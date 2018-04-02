4ed3cc0c9f7d5735b78a901a4acf3d9516f5761b
Mark <mark@Marks-MacBook-Pro.local>
2018-04-02 02:02:19 +0100

    Initial commit

---
+++ manifest.js
@@ -0,0 +1,6 @@
+module.exports = {
+  version: '1.0.0',
+  init: function (pluginContext) {
+    pluginContext.registerPolicy(require('./policies/tailor'))
+  }
+}
---
+++ policies/tailor.js
@@ -0,0 +1,45 @@
+module.exports = {
+  name: 'tailor',
+  schema: {
+    $id: 'http://express-gateway.io/schemas/policies/example-policy.json',
+    type: 'object',
+    properties: {
+      compositionUrl: {
+        type: 'string',
+        default: 'http://example.com'
+      }
+    }
+  },
+  policy: (actionParams) => {
+    return (req, res, next) => {
+      const _originalPipe = res.pipe
+      const _originalWriteHead = res.writeHead
+      const _originalWrite = res.write
+      const _originalEnd = res.end
+
+      let buffer = ''
+      let head
+      res.pipe = function (data) {
+        console.log('piped')
+      }
+      res.write = function (data) {
+        buffer += data
+      }
+      res.writeHead = function (data) {
+        head = data
+      }
+      res.end = function (data) {
+        buffer = 'INJECTED' + buffer
+
+        res.removeHeader('content-length')
+        res.removeHeader('x-gateway-layout')
+        res.removeHeader('x-powered-by')
+
+        _originalWriteHead.call(res, head)
+        _originalWrite.call(res, buffer)
+        _originalEnd.call(res)
+      }
+      next()
+    }
+  }
+}
